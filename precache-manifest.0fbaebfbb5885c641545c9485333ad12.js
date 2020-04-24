95296ed7aa5d9c24cfa3c49ff7761c3e62ab7a02
Matan Lurey <matan@lurey.org>
2020-04-24 15:08:33 -0700

    Updates

--- precache-manifest.0fbaebfbb5885c641545c9485333ad12.js
+++ precache-manifest.0fbaebfbb5885c641545c9485333ad12.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6729baef93ed1bec5ce80bc5f151341e",
+    "revision": "bc52ae49836b56dc761523e758d06fe4",
     "url": "/rollcrits/index.html"
   },
   {
-    "revision": "b94a8c21bd7a5da697fa",
+    "revision": "368c14f15709af514761",
     "url": "/rollcrits/static/css/2.cdf5201b.chunk.css"
   },
   {
-    "revision": "d202a2d068c7b8effc98",
+    "revision": "e0df6e182a2d290e2d7d",
     "url": "/rollcrits/static/css/main.37fe8b02.chunk.css"
   },
   {
-    "revision": "b94a8c21bd7a5da697fa",
-    "url": "/rollcrits/static/js/2.cc541448.chunk.js"
+    "revision": "368c14f15709af514761",
+    "url": "/rollcrits/static/js/2.a7811a6c.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/rollcrits/static/js/2.cc541448.chunk.js.LICENSE.txt"
+    "url": "/rollcrits/static/js/2.a7811a6c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d202a2d068c7b8effc98",
-    "url": "/rollcrits/static/js/main.1094a7a5.chunk.js"
+    "revision": "e0df6e182a2d290e2d7d",
+    "url": "/rollcrits/static/js/main.6cc4e33e.chunk.js"
   },
   {
     "revision": "843db664293cc4f573bd",
